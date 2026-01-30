e3251e15cea9b0732c48487861e298ce08d74945
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-29 23:54:29 -0500

    Update inputStyle.js

--- src/styles/inputStyle.js
+++ src/styles/inputStyle.js
@@ -91,7 +91,9 @@ export const inputStyle = (theme) => {
         borderColor: theme.palette.primary.light,
       },
 
-      
+      "&.Mui-focused": {
+  boxShadow: `0 0 0 2px ${theme.palette.primary.main}25`,
+},
 
       "&.Mui-focused fieldset": {
         borderColor: theme.palette.primary.main,
